1e13398aa160e9b18645457759df8e33911123c4
Preston Beachum <prestonbeachum@gmail.com>
2025-03-21 09:42:12 -0400

    Added comments to ClothingItem.java

--- src/ClothingItem.java
+++ src/ClothingItem.java
@@ -18,6 +18,17 @@ public class ClothingItem {
     private String material;
     private int quantity;
 
+    /**
+     * Constructs a new ClothingItem with the specified attributes.
+     *
+     * @param name the name of the clothing item
+     * @param color the color of the clothing item
+     * @param price the price of the clothing item
+     * @param size the size of the clothing item
+     * @param material the material of the clothing item
+     * @param quantity the quantity of the clothing item in stock
+     */
+
     public ClothingItem(String name, String color, double price, String size, String material, int quantity) {
         this.name = name;
         this.color = color;
@@ -27,50 +38,99 @@ public class ClothingItem {
         this.quantity = quantity;
     }
 
+    /**
+     *
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+
+    /**
+     *
+     * @return color
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     *
+     * @return price
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     *
+     * @return size
+     */
     public String getSize() {
         return size;
     }
 
+    /**
+     *
+     * @return material
+     */
     public String getMaterial() {
         return material;
     }
 
+    /**
+     *
+     * @return quantity
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     *
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     *
+     * @param color
+     */
     public void setColor(String color) {
         this.color = color;
     }
 
+    /**
+     *
+     * @param price
+     */
     public void setPrice(double price) {
         this.price = price;
     }
 
+    /**
+     *
+     * @param size
+     */
     public void setSize(String size) {
         this.size = size;
     }
 
+    /**
+     *
+     * @param material
+     */
     public void setMaterial(String material) {
         this.material = material;
     }
 
+    /**
+     *
+     * @param quantity
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
